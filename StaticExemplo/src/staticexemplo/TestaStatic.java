221d8d0f2624ec10dc18af31482f3178fa76af5b
Pedrinrxrx <pedrotorres.costaa@gmail.com>
2019-11-12 14:19:48 -0200

    Testando static

---
+++ StaticExemplo/src/staticexemplo/TestaStatic.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package staticexemplo;
+
+/**
+ *
+ * @author 1190142
+ */
+public class TestaStatic {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    static int  n1 = 572;
+    int n2 = 572;
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println(n1);
+        
+        TestaStatic ts = new TestaStatic ();
+        ts.n2 
+    }
+    
+}
