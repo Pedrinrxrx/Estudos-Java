3d40df323f49a577c95f0ad42f5a17f3e9c20070
PedroTorres10 <pedrotorres.costaa@gmail.com>
2020-11-23 12:29:39 -0200

    Testando interface mercado java

---
+++ InterfaceMercado/src/projetocadastrodeclentes/ProjetoCadastrodeClentes.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetocadastrodeclentes;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+/**
+ *
+ * @author Desktop
+ */
+public class ProjetoCadastrodeClentes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        CadastroCliente c  = new CadastroCliente();
+        c.setVisible(true);
+        
+        
+    }
+    
+    
+
+}
